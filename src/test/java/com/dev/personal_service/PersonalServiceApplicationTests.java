89a872b10600b935841df7326c3cbf4ed22124e9
v-lyutin <dyson-sv25@yandex.ru>
2024-09-29 16:05:49 +0300

    feat: initializing the project

---
+++ src/test/java/com/dev/personal_service/PersonalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.personal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
